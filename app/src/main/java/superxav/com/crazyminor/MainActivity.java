271e55f2fcbba2064ffd6b5858f6816233871adc
xavier Boutanquoy <Jorka@MacBook-Pro-de-xavier.local>
2017-03-10 23:15:31 +0100

    Initial commit

---
+++ app/src/main/java/superxav/com/crazyminor/MainActivity.java
@@ -0,0 +1,12 @@
+package superxav.com.crazyminor;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
